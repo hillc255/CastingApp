c9bc023bc241a68ec3ecc97382534d645c4e6b4d
Claudia Hill <hillc255@yahoo.com>
2021-03-08 20:58:09 -0500

    Added cors

---
+++ backend/src/config.py
@@ -0,0 +1,19 @@
+import os
+
+
+
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# DATABASE URL
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:picasso0@localhost:5432/castapp'
+
