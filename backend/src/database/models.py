db5f3935bb88f63a2f19dbb86b85020c2e0b8698
Claudia Hill <hillc255@yahoo.com>
2021-04-19 01:09:53 -0400

    Update

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -57,8 +57,9 @@ setup_db(app)
 
 #def setup_db(app, database_path=database_path):
 def setup_db(app): 
-    app.config["SQLALCHEMY_DATABASE_URI"] # = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] # = False
+    #app.config["SQLALCHEMY_DATABASE_URI"]
+    app.config["SQLALCHEMY_DATABASE_URI"] 
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]  = False
     db.app = app
     db.init_app(app)
 
