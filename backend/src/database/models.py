5dee7b547324ccc136c54121803f48fbaabd170c
Claudia Hill <hillc255@yahoo.com>
2021-04-23 06:16:57 -0400

    Update

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -15,9 +15,7 @@ print(f"**** models.py ****")
 
 db = SQLAlchemy()
 
-#DATABASE_URL = os.environ['DATABASE_URL']
-
-DATABASE_URL = os.environ['DB_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
@@ -32,7 +30,7 @@ print(f"1) DB_HOST={DB_HOST}")
 print(f"2) DB_USER={DB_USER}")
 print(f"3) DB_PASSWORD={DB_PASSWORD}")
 print(f"4) DB_NAME={DB_NAME}")
-print(f"5) DB_URL={DB_URL}")
+print(f"5) DB_URL={DATABASE_URL}")
 
 database_path = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
