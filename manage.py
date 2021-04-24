2e04afc68c348cf87060ec090224408971a66d34
Claudia Hill <hillc255@yahoo.com>
2021-04-24 08:31:58 -0400

    Update

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
-#from backend.src.database.models import db
+from backend.src.database.models import db
 
 # try:
 #     from flask_script import Manager
