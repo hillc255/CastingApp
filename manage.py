ac1dc1b4e123fc74c19c22024b0efda70611646b
Claudia Hill <hillc255@yahoo.com>
2021-04-24 08:18:21 -0400

    Update

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+ try:
+    from flask_script import Manager
+    from flask_migrate import Migrate, MigrateCommand
 
-from app import app
-from backend.src.database.models import db
+    from app import app
+    from backend.src.database.models import db
+except Exception as e:
+    print(e)
 
 print(f"**** manage.py ****")
 
-# app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 manager = Manager(app)
