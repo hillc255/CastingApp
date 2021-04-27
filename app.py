5aeda9cf824980f665e5c0169d0fff6c69409c77
Claudia Hill <hillc255@yahoo.com>
2021-04-27 04:15:12 -0400

    Update

--- app.py
+++ app.py
@@ -344,7 +344,7 @@ def create_app(test_config=None):
     # curl -X GET http://127.0.0.1:5000/movies/title  
     # '''
     
-    @app.route('/movies/search', methods=['GET'])
+    @app.route('/search', methods=['GET'])
     def findMovieByTitle():
 
         data = request.get_json()
