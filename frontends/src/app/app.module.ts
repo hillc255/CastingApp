d67c4010fabe37655daf06c044dbbb8dae2db88d
Claudia Hill <hillc255@yahoo.com>
2021-05-27 16:45:40 -0400

    Update

--- frontends/src/app/app.module.ts
+++ frontends/src/app/app.module.ts
@@ -7,9 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// Import the module from the SDK
-import { AuthModule } from '@auth0/auth0-angular';
-
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { AddMovieComponent } from './components/add-movie/add-movie.component';
@@ -17,6 +14,10 @@ import { AddMovieComponent } from './components/add-movie/add-movie.component';
 import { ActorsListComponent } from './components/actors-list/actors-list.component';
 import { ActorDetailsComponent } from './components/actor-details/actor-details.component';
 import { AddActorComponent } from './components/add-actor/add-actor.component';
+
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+
 
 @NgModule({
   declarations: [
@@ -30,14 +31,15 @@ import { AddActorComponent } from './components/add-actor/add-actor.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     // Import the module into the application, with configuration
     AuthModule.forRoot({
       domain: 'autumn-voice-0666.us.auth0.com',
       clientId: 'f7ZLU2DmWeRcLuikyEKjqk0893KA2Mbj'
     }),
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
