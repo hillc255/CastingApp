a6963551be8b99cd038c4a484487a2ebab0f1058
Claudia Hill <hillc255@yahoo.com>
2021-06-04 16:42:35 -0400

    Update

--- backend/src/config.py
+++ backend/src/config.py
@@ -12,10 +12,12 @@ DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # DATABASE URL
-DATABASE_URL = 'postgres://odsgqztiwmdgxb:fdba2f1e60653770f29df057aeedefd944889147582a10c8a4eb62883ab96ad6@ec2-544-167-152-185.compute-1.amazonaws.com:5432/d8gebjfq87smua'
+#old DATABASE_URL = 'postgres://odsgqztiwmdgxb:fdba2f1e60653770f29df057aeedefd944889147582a10c8a4eb62883ab96ad6@ec2-544-167-152-185.compute-1.amazonaws.com:5432/d8gebjfq87smua'
+DATABASE_URL = 'postgres://odsgqztiwmdgxb:fdba2f1e60653770f29df057aeedefd944889147582a10c8a4eb62883ab96ad6@ec2-54-167-152-185.compute-1.amazonaws.com:5432/d8gebjfq87smua'
 
 #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:picasso0@localhost:5432/castapp'
-SQLALCHEMY_DATABASE_URI = 'postgres://odsgqztiwmdgxb:fdba2f1e60653770f29df057aeedefd944889147582a10c8a4eb62883ab96ad6@ec2-544-167-152-185.compute-1.amazonaws.com:5432/d8gebjfq87smua'
+#old SQLALCHEMY_DATABASE_URI = 'postgres://odsgqztiwmdgxb:fdba2f1e60653770f29df057aeedefd944889147582a10c8a4eb62883ab96ad6@ec2-544-167-152-185.compute-1.amazonaws.com:5432/d8gebjfq87smua'
+SQLALCHEMY_DATABASE_URI = 'postgres://odsgqztiwmdgxb:fdba2f1e60653770f29df057aeedefd944889147582a10c8a4eb62883ab96ad6@ec2-54-167-152-185.compute-1.amazonaws.com:5432/d8gebjfq87smua'
 
 # class Config(object):
 #     DEBUG = False
