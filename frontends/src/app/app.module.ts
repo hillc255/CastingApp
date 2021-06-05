0a6b5a71252fc75a207e5bef97bb462f8d787a61
Claudia Hill <hillc255@yahoo.com>
2021-06-05 10:46:50 -0400

    Update

--- frontends/src/app/app.module.ts
+++ frontends/src/app/app.module.ts
@@ -50,7 +50,7 @@ import{ TokenInterceptor } from './token.interceptor';
       domain: 'autumn-voice-0666.us.auth0.com',
       clientId: 'f7ZLU2DmWeRcLuikyEKjqk0893KA2Mbj',
       audience: 'https://cast-app.herokuapp.com/api',
-      scope: 'openid profile email',
+      scope: 'openid',
       httpInterceptor: {
         allowedList: [
           {
