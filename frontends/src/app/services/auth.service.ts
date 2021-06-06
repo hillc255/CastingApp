70fbd1b649d3a05c554879d3b0a04ceb4dd03b1c
Claudia Hill <hillc255@yahoo.com>
2021-06-06 14:49:14 -0400

    Update

--- frontends/src/app/services/auth.service.ts
+++ frontends/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 //import { tokenNotExpired } from 'angular2-jwt';
 //import decode from 'jwt-decode';
-import * as auth0 from 'auth0-js';
+//import * as auth0 from 'auth0-js';
 //import * as Auth0 from 'auth0-web';
 import {JwtHelperService} from '@auth0/angular-jwt';
 
@@ -19,33 +19,7 @@ export class AuthService {
     // return a boolean reflecting 
     // whether or not the token is expired
     return helper.isTokenExpired(token);
+    //return false;
   }
-}
 
-  // export class AuthService {
-  //   authToken : any;
-  //   constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
-  //   loadToken(){
-  //       const token = localStorage.getItem('id_token');
-  //       this.authToken = token;
-  //       return this.authToken;
-  //   }
-  //   // Check if the token is Valid
-  //   loggedIn(){
-  //       this.authToken = this.loadToken();
-  //       console.log(this.jwtHelper.isTokenExpired(this.authToken));
-  // export class AuthService {
-    // authToken : any;
-    // constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
-    // loadToken(){
-    //     const token = localStorage.getItem('id_token');
-    //     this.authToken = token;
-    //     return this.authToken;
-    // }
-    // // Check if the token is Valid
-    // loggedIn(){
-    //     this.authToken = this.loadToken();
-    //     console.log(this.jwtHelper.isTokenExpired(this.authToken));
-    //     return this.jwtHelper.isTokenExpired(this.authToken);
-    // }
-  //   }
+}
