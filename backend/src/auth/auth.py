823d9744b121ee593630e5d14071c01ed3a4d796
Claudia Hill <hillc255@yahoo.com>
2021-06-05 20:34:20 -0400

    Update

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -60,7 +60,7 @@ def get_token_auth_header():
                             "Authorization header must be"
                             " Bearer token"}, 401)
 
-    token = parts[0]
+    token = parts[1]
     return token
 
 print("***Headers complete")
@@ -131,39 +131,39 @@ def requires_scope(required_scope):
                     return True
     return False
 
-print("***requires_scope complete")
+print("***requires_scope complete***")
 
-# PROTECT API ENDPOINTS
+# # PROTECT API ENDPOINTS
 
-# Controllers API
+# # Controllers API
 
-# This doesn't need authentication
-@APP.route("/api/public")
-@cross_origin(headers=["Content-Type", "Authorization"])
-def public():
-    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
-    return jsonify(message=response)
+# # This doesn't need authentication
+# @APP.route("/api/public")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# def public():
+#     response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+#     return jsonify(message=response)
 
-# This needs authentication
-@APP.route("/api/private")
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def private():
-    response = "Hello from a private endpoint! You need to be authenticated to see this."
-    return jsonify(message=response)
+# # This needs authentication
+# @APP.route("/api/private")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# @requires_auth
+# def private():
+#     response = "Hello from a private endpoint! You need to be authenticated to see this."
+#     return jsonify(message=response)
 
-# This needs authorization
-@APP.route("/api/private-scoped")
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def private_scoped():
-    if requires_scope("read:messages"):
-        response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
-        return jsonify(message=response)
-    raise AuthError({
-        "code": "Unauthorized",
-        "description": "You don't have access to this resource"
-    }, 403)
+# # This needs authorization
+# @APP.route("/api/private-scoped")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# @requires_auth
+# def private_scoped():
+#     if requires_scope("read:messages"):
+#         response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+#         return jsonify(message=response)
+#     raise AuthError({
+#         "code": "Unauthorized",
+#         "description": "You don't have access to this resource"
+#     }, 403)
 
 
 # RULE / ROLES
