aa80153ff8301acf768b69641ac9eee9830fd243
Claudia Hill <hillc255@yahoo.com>
2021-06-20 08:50:00 -0400

    Update

--- app.py
+++ app.py
@@ -109,8 +109,9 @@ def create_app(test_config=None):
 
 
     @app.route('/movies/<int:id>', methods=['GET'])
+    @requires_role(['director'||'assistant'])
     #@requires_role('director||assistant')
-    @requires_role('director','assistant')
+    #@requires_role('director','assistant')
     #@requires_role(['director','assistant'])
     def getMovie(id):
 
