fc5e3d133b93a117dc13bddccf7aa0534798b5ba
Claudia Hill <hillc255@yahoo.com>
2021-06-20 15:14:04 -0400

    Update

--- frontends/src/environments/environment.ts
+++ frontends/src/environments/environment.ts
@@ -1,12 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-//
-// export const environment = {
-//   production: false
-// };
-
-// environment.ts
 export const environment = {
   production: false,
   apiEndPoint: 'https://apiexample.com',
@@ -14,18 +5,9 @@ export const environment = {
   auth: {
     clientID: 'f7ZLU2DmWeRcLuikyEKjqk0893KA2Mbj',
     domain: 'autumn-voice-0666.us.auth0.com',
-    audience: 'https://cast-app.herokuapp.com/api', //'YOUR-AUTH0-API-IDENTIFIER', // e.g., http://localhost:3001
-    redirect: 'http://localhost:8081', //http://localhost:4200/callback',
+    audience: 'https://cast-app.herokuapp.com/api',
+    redirect: 'http://localhost:8081',
     scope: 'openid profile email',
     uri: 'https://cast-app.herokuapp.com/*'
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
