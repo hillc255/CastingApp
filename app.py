4d36c078e9a6702cc17eadce2e1f8401fdba386e
Claudia Hill <hillc255@yahoo.com>
2021-06-20 07:38:41 -0400

    Update

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def create_app(test_config=None):
 
 
     @app.route('/movies/<int:id>', methods=['GET'])
-    @requires_role(['director', 'assistant'])
+    @requires_role('director', 'assistant')
     def getMovie(id):
 
         if id is None:
