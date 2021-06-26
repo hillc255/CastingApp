7989b1e57d11a2354480e3177ba15a9a37a2f7a4
Claudia Hill <hillc255@yahoo.com>
2021-06-26 16:26:06 -0400

    Update

--- frontends/server.js
+++ frontends/server.js
@@ -5,11 +5,11 @@
  const app = express();
 
  // Serve only the static files form the dist directory
- app.use(express.static(__dirname + '/dist/frontends'));
+ app.use(express.static(__dirname + '/dist/frontends/src/'));
 
  app.get('/*', function(req,res) {
      
- res.sendFile(path.join(__dirname+'/dist/frontends/index.html'));
+ res.sendFile(path.join(__dirname+'/dist/frontends/src/index.html'));
  });
 
  // Start the app by listening on the default Heroku port
