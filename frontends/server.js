758a37e557b486571e833752d113e103d185d65c
Claudia Hill <hillc255@yahoo.com>
2021-06-26 08:20:02 -0400

    updates to deploy to heroku

--- frontends/server.js
+++ frontends/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/frontends'));
+app.use(express.static(__dirname + '/dist/CastingApp/frontends'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/frontends/index.html'));});
+'/dist/CastingApp/frontends/src/*'));});
 app.listen(process.env.PORT || 8080);
