f5d24748baa373b1d8e86de355da84385c6c7e4c
Claudia Hill <hillc255@yahoo.com>
2021-06-26 08:24:07 -0400

    Update

--- frontends/server.js
+++ frontends/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/CastingApp/frontends'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/CastingApp/frontends/src/*'));});
+'/dist/CastingApp/frontends/src/'));});
 app.listen(process.env.PORT || 8080);
