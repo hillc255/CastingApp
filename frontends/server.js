497c0900acbbb57c0251a576f9586ae01f70c2be
Claudia Hill <hillc255@yahoo.com>
2021-06-27 07:42:24 -0400

    updates to deploy to heroku

--- frontends/server.js
+++ frontends/server.js
@@ -6,10 +6,10 @@
  const app = express();
 
  // Serve only the static files from the dist directory
- app.use(express.static(__dirname + '/dist/cast-app'));
+ app.use(express.static(__dirname + '/dist/frontends'));
 
  app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/cast-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/frontends/index.html'));
  });
 
  // Start the app by listening on the default Heroku port
