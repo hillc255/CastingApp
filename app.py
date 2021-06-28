f3661639a7e17164822e030937a292847b202d2f
Claudia Hill <hillc255@yahoo.com>
2021-06-28 04:35:20 -0400

    Update

--- app.py
+++ app.py
@@ -670,6 +670,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-    # app.run()
-    # port = int(os.environ.get("PORT",5000))
-    # app.run(host='127.0.0.1',port=port,debug=True)
+
