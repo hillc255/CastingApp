1eb1efc5e3104f0ab30d17d789250b04f4a1f823
Claudia Hill <hillc255@yahoo.com>
2021-07-04 09:49:26 -0400

    Update

--- frontends/src/environments/environment.prod.ts
+++ frontends/src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   title: 'Production Environment Heading',
   userRole: 'https://cast-app.herokuapp.com/roles',
-  baseUrl: 'https://cast-app.herokuapp.com/movies',
-  baseUrl2: 'https://cast-app.herokuapp.com/actors',
+  baseUrl: 'https://cast-app.herokuapp.com/api/movies',
+  baseUrl2: 'https://cast-app.herokuapp.com/api/actors',
   auth: {
     clientID: 'f7ZLU2DmWeRcLuikyEKjqk0893KA2Mbj',
     domain: 'autumn-voice-0666.us.auth0.com',
     audience: 'https://cast-app.herokuapp.com/api',
-    redirect: 'https://cast-app.herokuapp.com/',
+    redirect: 'https://cast-app.herokuapp.com',
     scope: 'openid profile email',
     uri: 'https://cast-app.herokuapp.com/*'
   }
--- static/main.js
+++ static/main.js
@@ -593,8 +593,8 @@ const environment = {
     production: false,
     title: 'Local Environment Heading',
     userRole: 'https://cast-app.herokuapp.com/roles',
-    baseUrl: 'https://cast-app.herokuapp.com/movies',
-    baseUrl2: 'https://cast-app.herokuapp.com/actors',
+    baseUrl: 'https://cast-app.herokuapp.com/api/movies',
+    baseUrl2: 'https://cast-app.herokuapp.com/api/actors',
     auth: {
         clientID: 'f7ZLU2DmWeRcLuikyEKjqk0893KA2Mbj',
         domain: 'autumn-voice-0666.us.auth0.com',
