3bf76680a76e99d5b34057c125ce6f5d3bcc970c
Claudia Hill <hillc255@yahoo.com>
2021-07-06 04:41:27 -0400

    Update

--- static/main.js
+++ static/main.js
@@ -599,7 +599,7 @@ const environment = {
         clientID: 'f7ZLU2DmWeRcLuikyEKjqk0893KA2Mbj',
         domain: 'autumn-voice-0666.us.auth0.com',
         audience: 'https://cast-app.herokuapp.com/api',
-        redirect: 'https://cast-app.herokuapp.com',
+        redirect: 'http://localhost:8081',
         scope: 'openid profile email',
         uri: 'https://cast-app.herokuapp.com/*'
     }
